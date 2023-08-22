0390c33ff49a27e5f3f414e746937196f2ec15d1
Ryan Shane Palm <rspalm@bigpond.com>
2023-08-22 17:59:08 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -1,22 +1,35 @@
-import RPi.GPIO as GPIO
-import dht11
+import adafruit_dht
+import board
+import time
+
 
 class App:
 
+    def __init__(self):
+        self.dhtDevice = adafruit_dht.DHT11(board.D4)
+
     def init(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.cleanup()
 
-        # read data using pin 14
-        instance = dht11.DHT11(pin=14)
-        result = instance.read()
-
-        if result.is_valid():
-            print("Temperature: %-3.1f C" % result.temperature)
-            print("Humidity: %-3.1f %%" % result.humidity)
-        else:
-            print("Error: %d" % result.error_code)
+        while True:
+            try:
+                # Print the values to the serial port
+                temperature_c = self.dhtDevice.temperature
+                temperature_f = temperature_c * (9 / 5) + 32
+                humidity = self.dhtDevice.humidity
+                print(
+                    "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                        temperature_f, temperature_c, humidity
+                    )
+                )
+            except RuntimeError as error:
+                # Errors happen fairly often, DHT's are hard to read, just keep going
+                print(error.args[0])
+                time.sleep(2.0)
+                continue
+            except Exception as error:
+                self.dhtDevice.exit()
+                raise error
+            time.sleep(2.0)
 
 
 if __name__ == "__main__":
